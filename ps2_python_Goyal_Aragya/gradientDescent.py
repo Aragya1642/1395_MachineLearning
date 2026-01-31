f59e69bb1f2aa43bda796772f59bae7a4b48606c
Aragya Goyal <56979762+Aragya1642@users.noreply.github.com>
2026-01-31 14:53:40 -0500

    Finish implementation of gradientDescent

--- ps2_python_Goyal_Aragya/gradientDescent.py
+++ ps2_python_Goyal_Aragya/gradientDescent.py
@@ -1,2 +1,26 @@
-def gradientDescent(X, y, theta_init, alpha, num_iters):
-    pass
+import numpy as np
+from computeCost import computeCost
+
+def gradientDescent(X, y, theta_init, alpha, num_iters): 
+    # Initialize
+    theta = theta_init.copy()
+    cost_history = np.zeros((num_iters, 1))
+    m = X.shape[0]
+
+    # Perform gradient descent
+    for i in range(num_iters):
+        # Compute predictions and errors
+        hypothesis = X @ theta
+        err = hypothesis - y
+
+        # Calculate gradient
+        gradient = (1/m) * (np.transpose(X) @ err)
+        
+        # Calculate new thetas
+        theta = theta - (alpha * gradient)
+
+        # Calculate and save cost
+        cost_history[i] = computeCost(X, y, theta)
+    
+    # Return theta and cost history
+    return theta, cost_history
